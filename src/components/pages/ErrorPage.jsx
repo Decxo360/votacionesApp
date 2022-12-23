213a8b612714812a16b6930739c0e49631165269
Decxo360 <diegolundstedt99@gmail.com>
2022-12-23 01:34:26 -0300

    Todo: Mejorar estilo

---
+++ src/components/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ErrorPage = ({children}) => {
+  return (
+    <div>
+        <h1 className='text-red-700 text-[100px]' >Error, Esta ruta no existe.</h1>
+    </div>
+  )
+}
