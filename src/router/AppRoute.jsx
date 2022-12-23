e2517faa0d13fb7af1eabfe4e10ef10bc0c472a8
Decxo360 <diegolundstedt99@gmail.com>
2022-12-23 01:34:03 -0300

    creación de rutas

--- src/router/AppRoute.jsx
+++ src/router/AppRoute.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage,RegisterPage } from '../components/auth'
+import { ErrorPage, HomePage } from '../components/pages'
 
 export const AppRoute = () => {
   return (
-    <div>AppRoute</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/login'} element={<LoginPage/>}/>
+        <Route path={'/register'} element={<RegisterPage/>}/>
+        <Route path={'/Home'} element={<HomePage/>}/>
+        <Route path='/*' element={<ErrorPage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
