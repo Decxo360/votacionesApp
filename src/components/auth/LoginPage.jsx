77212baf5189c5563817965c6e488b12f7302160
Decxo360 <diegolundstedt99@gmail.com>
2022-12-23 01:32:47 -0300

    Edicion de estilos

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import { ButtomForm } from './components/ButtomForm'
 
 export const LoginPage = () => {
     return (
         <div className=' flex flex-col justify-center items-center w-[100%]'>
-            <div className=' flex flex-col justify-center items-center w-[50%]'>
-                <form action="">
-                    <label htmlFor=""></label>
-                    <input type="text" />
-                    <label htmlFor=""></label>
-                    <input type="text" name="" id="" />
+            <div className=' flex flex-col justify-center items-center w-[50%] h-[auto] mt-[200px] border bg-white shadow-2xl shadow-blue-500/50 rounded-lg'>
+                <form className='flex flex-col w-[80%]'>
+                    <label className='ml-5 mb-[-10px] z-10 w-[48px] bg-white'>Correo</label>
+                    <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" />
+                    <label className='ml-5 mb-[-10px] z-10 w-[80px] bg-white'>Contraseña</label>
+                    <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password"/>
                 </form>
-                <div>
-                    <h1>¿No has ingresado nunca? <Link to={'/'}> ¡registrate acá!</Link> </h1>
-                    <button className=' w-[50%] h-[50px] text-white' > Logearse </button>
-                </div>
+                <ButtomForm texto={'¿No has ingresado Nunca?'} url={'/register'} color={'bg-sky-800'} textBtn={'Login'}></ButtomForm>
             </div>
 
         </div>
--- src/components/auth/RegisterPage.jsx
+++ src/components/auth/RegisterPage.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { ButtomForm } from './components/ButtomForm'
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className='flex flex-col w-[100%] justify-center items-center mt-[100px]'>
+      <div className='flex flex-col w-[50%] justify-center items-center border bg-white shadow-2xl shadow-blue-500/50 rounded-lg'>
+        <form className='flex flex-col w-[80%]'>
+          <label className='ml-5 mb-[-10px] z-10 w-[65px] bg-white'>Nombre</label>
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name="" id="" />
+          <label className='ml-5 mb-[-10px] z-10 w-[70px] bg-white'>Apellidos</label>
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name="" id="" />
+          <label className='ml-5 mb-[-10px] z-10 w-[75px] bg-white'>Username</label>
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name="" id="" />
+          <label className='ml-5 mb-[-10px] z-10 w-[52px] bg-white'>Correo</label>
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="email" name="" id="" />
+          <label className='ml-5 mb-[-10px] z-10 w-[86px] bg-white'>Contraseña</label>
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" name="" id="" />
+          <label className='ml-5 mb-[-10px] z-10 w-[95px] bg-white'>Contraseña 2</label>
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" name="" id="" />
+        </form>
+        <ButtomForm texto={'¿Ya tienes una cuenta?'} url={'/login'} color={'bg-indigo-800'} textBtn={'Registrarse'}></ButtomForm>
+      </div>
+    </div>
   )
 }
