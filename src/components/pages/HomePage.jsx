8e3ff30c021eea5b0fc17918cc47592b49caf829
Decxo360 <diegolundstedt99@gmail.com>
2022-12-26 21:55:52 -0300

    vista de componentes, todo: borrar componentes

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { NavBar } from '../NavBar'
+import { MiniPerfil } from './components/MiniPerfil'
+import { Perfil } from './components/Perfil'
+import { VotacionCard } from './components/VotacionCard'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <NavBar/>
+      <MiniPerfil/>
+      <VotacionCard/>
+      <Perfil/>
+    </div>
   )
 }
