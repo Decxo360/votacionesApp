c18b89157d6ab494e67f2310cf3869898a6f80d1
Decxo360 <diegolundstedt99@gmail.com>
2022-12-26 21:56:17 -0300

    Prueba de como se vería con sus componentes

--- src/components/pages/ProfilePage.jsx
+++ src/components/pages/ProfilePage.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { MiniPerfil } from './components/MiniPerfil'
+import { Perfil } from './components/Perfil'
+import { VotacionCard } from './components/VotacionCard'
 
 export const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <div className='flex flex-col'>
+      <Perfil/>
+      <div className='flex flex-row ml-10 justify-around mt-10'>
+        <div className='flex flex-wrap gap-3 justify-center'>
+          <VotacionCard/>
+          <VotacionCard/>
+          <VotacionCard/>
+          <VotacionCard/>
+          <VotacionCard/>
+          <VotacionCard/>
+          <VotacionCard/>
+        </div>
+        <div>
+          <div className='flex flex-col w-[400px] border rounded-xl justify-center items-center '>
+            <MiniPerfil/>
+            <MiniPerfil/>
+            <MiniPerfil/>
+            <MiniPerfil/>
+            <MiniPerfil/>
+            <MiniPerfil/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
