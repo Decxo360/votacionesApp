61b9d3ccdcd2ee91330d09b9f0a041dc60c01a38
Decxo360 <diegolundstedt99@gmail.com>
2022-12-26 21:55:24 -0300

    nuevo estilo

--- src/components/auth/components/ButtomForm.jsx
+++ src/components/auth/components/ButtomForm.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export const ButtomForm = ({texto, metodo, color, url,textBtn}) => {
   return (
     <div>
-        <h1 className='mb-[20px] mt-[20px]'>{texto}<Link className='hover:text-orange-800 scale-110 duration-100 transition delay-150' to={url} > ¡acá te ayudamos! </Link></h1>
+        <h1 className='mb-[20px] mt-[20px]'>{texto}<Link className='hover:text-amber-500 hover:scale-150 transition ease-in-out delay-100' to={url} > ¡acá te ayudamos! </Link></h1>
         <button className={`ml-3 rounded-3xl w-[95%] h-[50px] hover:bg-blue-700 text-white focus:bg-blue-700 mb-[20px] ${color}` }>{textBtn}</button>
     </div>
   )
