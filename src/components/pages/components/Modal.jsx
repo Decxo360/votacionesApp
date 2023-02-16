da79f0931ff03b9ef103cb00cdf8a1bf36b40420
Decxo360 <diegolundstedt99@gmail.com>
2023-02-16 19:09:09 -0300

    eliminacion de lógica, hacerlo mas sencillo

--- src/components/pages/components/Modal.jsx
+++ src/components/pages/components/Modal.jsx
@@ -1,8 +1,8 @@
 import React, { Children } from 'react'
 
-export const Modal = ({children, className,isActive}) => {
+export const Modal = ({children, className }) => {
   return (
-    <div className={`w-[100%] h-[100%] fixed top-0 left-0 z-[1] bg-[rgba(0,0,0,0.5)] ${className} ${isActive}`}>
+    <div className={`w-[100%] h-[100%] fixed top-0 left-0 z-[1] bg-[rgba(0,0,0,0.5)] ${className}`}>
        <div className='flex flex-col justify-center items-center w-[100%] h-[100%]'>
         {children}
        </div>
