3f6a2968d94ef1aaba9d6f205507db0e6a1d5591
Decxo360 <diegolundstedt99@gmail.com>
2023-02-15 21:18:58 -0300

    obtencion de los valores del formulario

--- src/components/auth/LoginPage.jsx
+++ src/components/auth/LoginPage.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ButtomForm } from './components/ButtomForm'
 
 export const LoginPage = () => {
+
+    const [form, setform] = useState({
+        correo:'',
+        contrasena:''
+    })
+
+    const onChangeCorreo =(event)=>{
+        event.preventDefault()
+        setform({...form, correo: event.target.value})
+    }
+    const onChangeContrasena =(event)=>{
+        event.preventDefault()
+        setform({...form, contrasena: event.target.value})
+    }
+
+    console.log(form);
+
     return (
         <div className=' flex flex-col justify-center items-center w-[100%]'>
             <div className=' flex flex-col justify-center items-center w-[50%] h-[auto] mt-[200px] border bg-white shadow-2xl shadow-blue-500/50 rounded-lg'>
                 <form className='flex flex-col w-[80%]'>
                     <label className='ml-5 mb-[-10px] z-10 w-[48px] bg-white'>Correo</label>
-                    <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" />
+                    <input className='pl-5 border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text"  onChange={onChangeCorreo}/>
                     <label className='ml-5 mb-[-10px] z-10 w-[80px] bg-white'>Contraseña</label>
-                    <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password"/>
+                    <input className='pl-5 border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" onChange={onChangeContrasena}/>
                 </form>
                 <ButtomForm texto={'¿No has ingresado Nunca?'} url={'/register'} color={'bg-sky-800'} textBtn={'Login'}></ButtomForm>
             </div>
--- src/components/auth/RegisterPage.jsx
+++ src/components/auth/RegisterPage.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
 import { ButtomForm } from './components/ButtomForm'
 
+
 export const RegisterPage = () => {
+  
+const [register, setRegister] = useState({nombre:'',apellidos:'',username:'',correo:'',contrasena:'',contrasena2:''})
+
+const onHandleChange=(event)=>{
+  event.preventDefault()
+  const {name,value} = event.target
+  setRegister({
+    ...register,
+    [name]:value
+  })
+}
+
   return (
     <div className='flex flex-col w-[100%] justify-center items-center mt-[100px]'>
       <div className='flex flex-col w-[50%] justify-center items-center border bg-white shadow-2xl shadow-blue-500/50 rounded-lg'>
         <form className='flex flex-col w-[80%]'>
           <label className='ml-5 mb-[-10px] z-10 w-[65px] bg-white'>Nombre</label>
-          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name="" id="" />
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name='nombre' onChange={onHandleChange} />
           <label className='ml-5 mb-[-10px] z-10 w-[70px] bg-white'>Apellidos</label>
-          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name="" id="" />
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name='apellidos' onChange={onHandleChange} />
           <label className='ml-5 mb-[-10px] z-10 w-[75px] bg-white'>Username</label>
-          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name="" id="" />
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="text" name='username' onChange={onHandleChange} />
           <label className='ml-5 mb-[-10px] z-10 w-[52px] bg-white'>Correo</label>
-          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="email" name="" id="" />
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="email" name='correo' onChange={onHandleChange} />
           <label className='ml-5 mb-[-10px] z-10 w-[86px] bg-white'>Contraseña</label>
-          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" name="" id="" />
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" name='contrasena' onChange={onHandleChange} />
           <label className='ml-5 mb-[-10px] z-10 w-[95px] bg-white'>Contraseña 2</label>
-          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" name="" id="" />
+          <input className='border h-[50px] border-sky-800 focus:outline-sky-800 placeholder:italic' type="password" name='contrasena2' onChange={onHandleChange} />
         </form>
         <ButtomForm texto={'¿Ya tienes una cuenta?'} url={'/login'} color={'bg-indigo-800'} textBtn={'Registrarse'}></ButtomForm>
       </div>
