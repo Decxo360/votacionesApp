c70d3888f9b7f848e9d0e053e5e4315581d0982c
Decxo360 <diegolundstedt99@gmail.com>
2023-02-15 22:30:22 -0300

    cambios de estilos

--- src/components/pages/components/VotacionCard.jsx
+++ src/components/pages/components/VotacionCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const VotacionCard = ({titulo='Votacion Presidencial',creador='Sebastian Piñera'}) => {
   return (
-    <div className='flex flex-col border rounded-xl w-[200px] h-[100px] justify-center items-center sm:w-[10%] md:w-[25%] hover:cursor-pointer hover:bg-slate-200'>
+    <div className='flex flex-col border rounded-xl w-[200px] h-[100px] justify-center items-center sm:w-[50%] md:w-[35%] hover:cursor-pointer hover:bg-slate-200'>
         <h1 className='text-black text-[20px]'>{titulo}</h1>
         <span className='text-slate-500'>By: {creador}</span>
     </div>
