18e7cec523b0f7dc3e4ca96c965573673c2409be
Decxo360 <diegolundstedt99@gmail.com>
2023-02-16 19:07:45 -0300

    nueva ruta, para votar las votaciones

--- src/router/AppRoute.jsx
+++ src/router/AppRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { LoginPage,RegisterPage } from '../components/auth'
 import { ErrorPage, HomePage, ProfilePage, SearchPage, VotePage } from '../components/pages'
+import { VotarPage } from '../components/pages/Votar/VotarPage'
 
 export const AppRoute = () => {
   return (
@@ -12,7 +13,8 @@ export const AppRoute = () => {
         <Route path={'/Home'} element={<HomePage/>}/>
         <Route path={'/Search'} element={<SearchPage/>}/>
         <Route path={'/Perfil/:id'} element={<ProfilePage/>}/>
-        <Route path={'/Votacion'} element={<VotePage/>}/>
+        <Route path={'/Votacion/:titulo'} element={<VotePage/>}/>
+        <Route path={'/Votacion/votar/:titulo'} element={<VotarPage/>}/>
         <Route path='/*' element={<ErrorPage/>}/>
       </Routes>
     </BrowserRouter>
