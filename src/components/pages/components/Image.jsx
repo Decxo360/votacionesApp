90fa61e1525691bd9d9a5eb36d3881f806a13b40
Decxo360 <diegolundstedt99@gmail.com>
2023-02-15 22:30:54 -0300

    valores por defecto, y nuevas funcionalidades

--- src/components/pages/components/Image.jsx
+++ src/components/pages/components/Image.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Image = ({url='https://cdn-icons-png.flaticon.com/512/64/64572.png',name='logo',className='rounded-full'}) => {
+export const Image = ({url='https://cdn-icons-png.flaticon.com/512/64/64572.png',name='logo',className='rounded-full',method}) => {
   return (
-    <img className={className} src={url} alt={name} />
+    <img className={className} src={url} alt={name} onClick={method} />
   )
 }
