df9e529b34ff2309aea3371d546eb1b435c86893
Decxo360 <diegolundstedt99@gmail.com>
2023-02-15 22:29:47 -0300

    cambio de clases y props

--- src/components/pages/components/Btn.jsx
+++ src/components/pages/components/Btn.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Btn = ({className, texto, method}) => {
   return (
-    <button className={`text-white w-[100px] h-[40px] rounded-xl ${className}`} onClick={method}>{texto}</button>
+    <button className={`rounded-xl ${className}`} onClick={method}>{texto}</button>
   )
 }
