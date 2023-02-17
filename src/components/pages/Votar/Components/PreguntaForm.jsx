c6ed93bee8019754fe6fbcc7b3a6ee91a8d488d5
Decxo360 <diegolundstedt99@gmail.com>
2023-02-16 21:58:22 -0300

    funcionalidad removida e importada

--- src/components/pages/Votar/Components/PreguntaForm.jsx
+++ src/components/pages/Votar/Components/PreguntaForm.jsx
@@ -1,21 +1,25 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import { useVotar } from '../../../../hooks/useVotar'
 import { Alternativa } from './Alternativa'
 
-export const PreguntaForm = ({pregunta,numero}) => {
+let alternativas = [
+  { numero: 1, texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur vero autem eius qui labore, unde rerum doloribus deserunt laboriosam amet sit perspiciatis ab voluptas aliquam placeat provident! Obcaecati, architecto.' },
+  { numero: 2, texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur vero autem eius qui labore, unde rerum doloribus deserunt laboriosam amet sit perspiciatis ab voluptas aliquam placeat provident! Obcaecati, architecto.' },
+  { numero: 3, texto: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit consectetur vero autem eius qui labore, unde rerum doloribus deserunt laboriosam amet sit perspiciatis ab voluptas aliquam placeat provident! Obcaecati, architecto.' },
+]
 
-    const div = useRef()
-    const handleClick = ()=>{
-        console.log(div.current.children[0]);
-    }
-
+export const PreguntaForm = ({ pregunta, numero }) => {
+  const {inputs,div,handleChangeInput,handleClickInput} = useVotar()
   return (
     <div className='w-[60%] border mt-5 rounded-xl pb-[20px]'>
-        <h1 className='text-[25px] ml-5'>{numero}) {pregunta}</h1>
-        <div className='ml-5 mt-5 flex flex-col gap-5' ref={div} onClick={handleClick}>
-            <Alternativa texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. In autem eligendi saepe doloremque ex sit explicabo, cumque rerum impedit reprehenderit obcaecati odio recusandae ea soluta, temporibus voluptatum quae nisi debitis.'} numero={1}/>
-            <Alternativa texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. In autem eligendi saepe doloremque ex sit explicabo, cumque rerum impedit reprehenderit obcaecati odio recusandae ea soluta, temporibus voluptatum quae nisi debitis.'} numero={2}/>
-            <Alternativa texto={'Lorem ipsum dolor sit amet consectetur adipisicing elit. In autem eligendi saepe doloremque ex sit explicabo, cumque rerum impedit reprehenderit obcaecati odio recusandae ea soluta, temporibus voluptatum quae nisi debitis.'} numero={3}/>
-        </div>
+      <h1 className='text-[25px] ml-5'>{numero}) {pregunta}</h1>
+      <div className='ml-5 mt-5 flex flex-col gap-5' ref={div}>
+        {
+          alternativas.map(({ texto, numero }, index) => (
+            <Alternativa index={index} handleClick={handleClickInput} handleChangeInput={handleChangeInput} texto={texto} numero={numero} key={index} referencia={(el) => (inputs.current[index] = el)} />
+          ))
+        }
+      </div>
     </div>
   )
 }
