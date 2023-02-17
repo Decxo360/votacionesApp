cb6de1e8bf0e7d2c2c0b385fd4b3f4aa5c227b7f
Decxo360 <diegolundstedt99@gmail.com>
2023-02-16 21:57:45 -0300

    nuevo componente agregado

--- src/components/pages/Votar/VotarPage.jsx
+++ src/components/pages/Votar/VotarPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { Btn } from '../components/Btn'
 import { PreguntaForm } from './Components/PreguntaForm'
 
 export const VotarPage = () => {
@@ -12,7 +13,9 @@ export const VotarPage = () => {
         <div className='flex flex-col justify-center items-center w-[100%]'>
           <PreguntaForm numero={1} pregunta={'Que tu hace'}/>
           <PreguntaForm numero={1} pregunta={'Que tu hace'}/>
+          <PreguntaForm numero={1} pregunta={'Que tu hace'}/>
         </div>
+        <Btn texto='Enviar los votos' className={'w-auto bg-blue-700 text-white h-[45px] pr-5 pl-5 mt-10 mb-10 hover:cursor-pointer hover:bg-blue-600'}/>
     </div>
   )
 }
